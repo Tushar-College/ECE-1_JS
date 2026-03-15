8cb87cb6702c379bd5ef8c07cdab39e785c0f309
Tushar <educatetushar@gmail.com>
2026-03-15 22:51:29 +0530

    Add JavaScript object examples

---
+++ object.js
@@ -0,0 +1,19 @@
+var person = {
+    name: 'John',
+    age: 30,
+    isMarried: true,
+    address:{
+        street:'vinay nagar',
+        flat: 'c/34'
+    }
+}
+
+
+// or
+
+
+var person2=new Object();
+person2.name="krish";
+person2.age=32;
+person2.isMarried=true;
+person2.address={};
