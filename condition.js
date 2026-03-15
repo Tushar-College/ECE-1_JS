822391b43a428835923761a7f9b5f35a9c6bc175
Tushar <educatetushar@gmail.com>
2026-03-15 22:57:26 +0530

    Add JavaScript conditional statements example

---
+++ condition.js
@@ -0,0 +1,14 @@
+let marks = prompt("Enter your marks:");
+marks = Number(marks); 
+if (marks >= 90) {
+    console.log("Grade: A");
+} 
+else if (marks >= 75) {
+    console.log("Grade: B");
+} 
+else if (marks >= 50) {
+    console.log("Grade: C");
+} 
+else {
+    console.log("Fail");
+}
